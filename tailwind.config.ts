c7a721bc828e911ca09efc3a4cd3b8254b5065d0
Daniel Rashba <danirdd92@gmail.com>
2023-05-15 23:58:32 +0300

    example tw styles [todo - continue where i left]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
   content: [
@@ -7,7 +8,44 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["'Noto Sans Hebrew'", ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      aqua: '#00FFAA',
+      'aqua-bright': '#7CFFD3',
+      violet: '#8146FF',
+      white: '#fff',
+      'white-50': 'rgb(var(--color-white) / 0.5)',
+      black: '#000',
+    },
+
+    extend: {
+      fontSize: {
+        'section-title': [
+          '4rem',
+          {
+            lineHeight: '169%',
+            fontWeight: 900,
+          },
+        ],
+        'header-footer': [
+          '1.25rem',
+          {
+            fontWeight: 700,
+          },
+        ],
+        'section-subtitle': '2rem',
+        quota: [
+          '3rem',
+          {
+            fontWeight: 900,
+          },
+        ],
+        'body-title': '1.75rem',
+        'body-text': '1.625rem',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
