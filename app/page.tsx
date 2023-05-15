f10ad4cd53cf60f4ab09a1ed5ac1ad80bbca2c40
Daniel Rashba <danirdd92@gmail.com>
2023-05-15 20:57:20 +0300

    fixed faulty template

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-      {" "}
-    </main>
-  );
-}
