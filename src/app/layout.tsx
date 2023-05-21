8f881c5339f963d820fd4a330f44e3570c318041
maor meir hajaj m1pro <hajajmaor@gmail.com>
2023-05-21 22:20:42 +0300

    set mx globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' mx-8'}>
+      <body className={inter.className + ' mx-16 lg:mx-32'}>
         <Header />
         {children}
       </body>
