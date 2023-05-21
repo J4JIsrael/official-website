b499d6ef919347cd26f31a11f329db8959887ddd
maor meir hajaj m1pro <hajajmaor@gmail.com>
2023-05-21 22:20:12 +0300

    create people graph cmp

---
+++ src/components/main/PeopleGraph.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export const PeopleGraph = () => (
+  // svg from file:
+  // public/assets/peopleGraph.svg
+  <Image
+    src="/assets/peopleGraph.svg"
+    alt="people graph"
+    fill
+    style={{
+      objectFit: 'cover',
+    }}
+  />
+);
