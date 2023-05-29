2c50183928fc7b68963045242b9607fe281e2ded
Asaf Levi <asafl14@yahoo.com>
2023-05-29 20:10:56 +0300

    State update by onClick each button, using union.

--- src/components/activitiesHeadlines/HeadLines.tsx
+++ src/components/activitiesHeadlines/HeadLines.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import { HeadlineBtn } from './HeadlineBtn'
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import { HeadlineBtn } from './HeadlineBtn';
 
 export const HeadLines = () => {
+  type btnSelected = 'btnL' | 'btnCenter' | 'btnR'; 
+  const [isSelected, setIsSelected] = useState<btnSelected>('btnCenter');
 
-  const myClass = `absolute w-44 h-16 select-none overflow-hidden flex-row justify-center items-center 
-  opacity-40 hover:rounded-3xl hover:h-20 hover:opacity-80 hover:scale-110 focus:z-10  transition-all duration-200`;
+  const onClick = (type: btnSelected) => {
+    setIsSelected(type);
+  };
 
-    const idHandler = (event: React.MouseEvent<HTMLElement>) => {
-
-  }
-
+  useEffect(() => {
+    console.log(isSelected);
+  }, [isSelected]);
 
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <h2 className='text-white text-3xl font-bold'>הפעילות שלנו</h2>
-    <div className='relative flex justify-center gap-2 items-center w-11/12 h-32 '>    
-        <HeadlineBtn myClass={`${myClass}  rounded-l-[5rem] rounded-r-[1rem] relative`} text='העשרה'/>
-        <HeadlineBtn myClass={`${myClass}  rounded-l-[1rem]  rounded-r-[1rem] relative`} text='פרויקטים'/>
-        <HeadlineBtn myClass={`${myClass}  rounded-l-[1rem] rounded-r-[5rem]  relative`} text='מציאת עבודה'/>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-3xl font-bold text-white">הפעילות שלנו</h2>
+      <div className="relative flex h-32 w-11/12 items-center justify-center gap-2 ">
+        <HeadlineBtn
+          onClick={() => setIsSelected('btnL')}
+          className={`relative rounded-l-[5rem] rounded-r-[1rem]`}
+          text="העשרה"
+        />
+        <HeadlineBtn
+          onClick={() => setIsSelected('btnCenter')}
+          className={`relative  rounded-l-[1rem] rounded-r-[1rem]`}
+          text="פרויקטים"
+        />
+        <HeadlineBtn
+          onClick={() => setIsSelected('btnR')}
+          className={`relative rounded-l-[1rem]  rounded-r-[5rem]`}
+          text="מציאת עבודה"
+        />
+      </div>
+      <h3 className="text-white">{isSelected}</h3>
     </div>
-    </div>
-  )
-}
+  );
+};
--- src/components/activitiesHeadlines/HeadlineBtn.tsx
+++ src/components/activitiesHeadlines/HeadlineBtn.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import React, { forwardRef } from 'react';
 
 interface HeadlineBtnProps {
-    text:string;
-    myClass: string;
+  text: string;
+  className: string;
+  onClick: () => void; 
 }
 
-
-
-
-export const HeadlineBtn: React.FC<HeadlineBtnProps> = ({ text, myClass}) => {
-
-
+export const HeadlineBtn = ({ text, className, onClick }: HeadlineBtnProps) => {
   return (
-
-    <div className={myClass}>
-      <div className='absolute w-full h-full bg-gradient-to-r from-violet to-aqua-bright flex items-center justify-center'>
-        <h3 className='absolute bg-opacity-0 text-white font-sans font-bold text-lg'>{text}</h3>
+    <div
+      onClick={onClick}
+      className={`absolute h-16 w-44 select-none flex-row items-center justify-center overflow-hidden 
+      opacity-40 transition-all duration-200 hover:h-20 hover:scale-110 hover:rounded-3xl  hover:opacity-80 focus:z-10 ${className}`}
+    >
+      <div className="absolute flex h-full w-full items-center justify-center bg-gradient-to-r from-violet to-aqua-bright">
+        <h3 className="absolute bg-opacity-0 font-sans text-lg font-bold text-white">
+          {text}
+        </h3>
       </div>
     </div>
-
   );
 };
