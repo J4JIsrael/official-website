f64941c12ca71f141bad834a80e54a5699222408
Irad Doron <irad.doron@gmail.com>
2023-05-29 20:39:48 +0300

    finish footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,13 +3,16 @@ import { LogoBlock } from './LogoBlock';
 import { PlatformsBlock } from './PlatformsBlock';
 
 export const Footer = () => {
-  const baseStyles =
-    'bg-gray-100 text-gray-700 flex h-20 w-full flex-row items-center justify-between p-20';
-
   // TODO: add backgorund from Tailwind theme
-  
+
   return (
-    <footer className={`${baseStyles} opacity-70`}>
+    <footer
+      className="bg-footer-gradient flex w-full flex-row items-center justify-between p-20 opacity-70 max-lg:flex-col max-lg:gap-6 max-lg:p-6"
+      style={{
+        background:
+          'linear-gradient(324.86deg, rgba(0, 255, 157, 0.1) -107.86%, rgba(213, 93, 255, 0.2) 176.56%)',
+      }}
+    >
       <LogoBlock />
       <CenterBlock />
       <PlatformsBlock />
