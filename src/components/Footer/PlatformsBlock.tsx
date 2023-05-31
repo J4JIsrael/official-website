83ed8b0c2cd2d81cfc3d3e9481699ee9b6331392
Irad Doron <irad.doron@gmail.com>
2023-05-31 11:05:05 +0300

    fix icons PR comment

--- src/components/Footer/PlatformsBlock.tsx
+++ src/components/Footer/PlatformsBlock.tsx
@@ -3,38 +3,34 @@ import { FacebookIcon } from '@/components/icons/FacebookIcon';
 import { LinkedinIcon } from '@/components/icons/LinkedinIcon';
 import { WhatsappIcon } from '@/components/icons/WhatsappIcon';
 import { YoutubeIcon } from '@/components/icons/YoutubeIcon';
-import { LinkIcon } from './LinkIcon';
 
 export const PlatformsBlock = () => {
-  const icons = [
-    {
-      icon: <FacebookIcon />,
-      linkSrc: 'https://www.facebook.com/groups/5915476325238538',
-    },
-    {
-      icon: <DiscordIcon />,
-      linkSrc: 'https://www.linkedin.com/groups/9305833/',
-    },
-    {
-      icon: <LinkedinIcon />,
-      linkSrc: 'https://www.youtube.com/channel/UCSezD9Ad0PNbwI0I9a3mztA',
-    },
-    {
-      icon: <WhatsappIcon />,
-      linkSrc: 'https://chat.whatsapp.com/G7c35OuBRtgC6g4ytOI1te',
-    },
-    {
-      icon: <YoutubeIcon />,
-      linkSrc: 'https://discord.gg/4VJu5cWKv9',
-    },
-  ];
-
   return (
     <div className="flex flex-row items-center justify-evenly gap-2">
-      {icons.map((linkIcon, index) => {
-        const { icon, linkSrc } = linkIcon;
-        return <LinkIcon key={index} linkSrc={linkSrc} icon={icon} />;
-      })}
+      <a
+        href="https://www.facebook.com/groups/5915476325238538"
+        target="_blank"
+      >
+        <FacebookIcon />
+      </a>
+      <a href="https://www.linkedin.com/groups/9305833/" target="_blank">
+        <LinkedinIcon />
+      </a>
+      <a
+        href="https://chat.whatsapp.com/G7c35OuBRtgC6g4ytOI1te"
+        target="_blank"
+      >
+        <WhatsappIcon />
+      </a>
+      <a href="https://discord.gg/4VJu5cWKv9" target="_blank">
+        <DiscordIcon />
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UCSezD9Ad0PNbwI0I9a3mztA"
+        target="_blank"
+      >
+        <YoutubeIcon />
+      </a>
     </div>
   );
 };
