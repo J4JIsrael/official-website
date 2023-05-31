3eb53f3e2e796c6332d8e134463f9e33c532978a
Asaf Levi <asafl14@yahoo.com>
2023-05-31 17:15:21 +0300

    Headlines order fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import { HeadLines } from '@j4j/components';
+import { HeadLines, Main } from '@j4j/components';
 
 export default function Home() {
   return (
     <>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold tracking-widest">Welcome to J4J</h1>
-      <HeadLines />
-    </main>
-        <Main />
-      </>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-3xl font-bold tracking-widest">Welcome to J4J </h1>
+        <HeadLines />
+      </main>
+      <Main />
+    </>
+  );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './header';
 export * from './icons';
 export  * from './headLines';
+export  * from './main';
