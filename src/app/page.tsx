241d492bfa580e5a8fad19f7c142c48a340fe780
DoovDevan <haleviamit@gmail.com>
2023-06-01 18:49:59 +0300

    CommunitySentence Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Main } from '@/components/main';
+import { CommunitySentence } from '@j4j/components';
 
 export default function Home() {
   return (
     <>
       <Main />
+      <CommunitySentence />
     </>
   );
 }
