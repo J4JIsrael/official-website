f2fc1c57a92d4c5be60930e96c40bcd423ab5161
Irad Doron <irad.doron@gmail.com>
2023-06-03 22:18:09 +0300

    fix bug

--- src/components/goalIcons/GoalIcons.tsx
+++ src/components/goalIcons/GoalIcons.tsx
@@ -1,11 +1,9 @@
+import { MailIcon, PeopleIcon, StudiesIcon } from '../icons';
 import { GoalIcon } from './GoalIcon';
-import { MailIcon } from '../icons';
-import { PeopleIcon } from '../icons';
-import { StudiesIcon } from '../icons';
 
 export const GoalIcons = () => {
   return (
-    <div className="flex w-full  items-center justify-around">
+    <div className="flex w-full items-center justify-around xs:mb-10 xs:flex-col xs:gap-16 sm:mb-10 sm:flex-col sm:gap-16 md:mb-10 md:flex-col md:gap-16 lg:flex-row">
       <GoalIcon svgIcon={<MailIcon />} />
       <GoalIcon svgIcon={<PeopleIcon />} />
       <GoalIcon svgIcon={<StudiesIcon />} />
