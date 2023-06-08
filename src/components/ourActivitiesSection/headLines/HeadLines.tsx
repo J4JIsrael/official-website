7fc74986a86ee5074cf24dc61f1c73287cda7872
Irad Doron <irad.doron@gmail.com>
2023-06-08 19:16:17 +0300

    fix classes bug

--- src/components/ourActivitiesSection/headLines/HeadLines.tsx
+++ src/components/ourActivitiesSection/headLines/HeadLines.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { type ActivitiesLabel } from '@/types';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { HeadlineBtn } from './HeadlineBtn';
 
 interface HeadLinesProps {
@@ -9,67 +9,43 @@ interface HeadLinesProps {
 }
 
 export const HeadLines = ({ setActiveLabel, activeLabel }: HeadLinesProps) => {
-  type btnSelected = 'btnL' | 'btnCenter' | 'btnR';
-  const [isSelected, setIsSelected] = useState<btnSelected>('btnCenter');
-
-  const [btnLSelected, setBtnLSelected] = useState('');
-  const [btnCenter, setBtnCenterSelected] = useState('');
-  const [btnR, setBtnRSelected] = useState('');
-
-  const selectedClass =
-    'h-20 scale-110  opacity-80  rounded-l-2xl rounded-r-2xl'; //scale-x-110
-
-  const onClick = (type: btnSelected, newActiveLabel: ActivitiesLabel) => {
-    setActiveLabel(newActiveLabel);
-    setIsSelected(type);
-  };
-
-  const getClassesSelecedTab = (
+  const getClassesForTabBtn = (
     activeLabel: ActivitiesLabel,
     currentTabLabel: ActivitiesLabel
   ) => {
-    return activeLabel === currentTabLabel ? selectedClass : '';
-  };
+    const isSelected = activeLabel === currentTabLabel;
+    const selectedClass =
+      'h-20 scale-110  opacity-80 rounded-l-2xl rounded-r-2xl border-2 border-white border-opacity-50';
+    const defaultTabsClasses = {
+      education: 'relative cursor-pointer rounded-l-[1rem] rounded-r-[5rem]',
+      jobs: 'relative cursor-pointer rounded-md',
+      projects: 'relative cursor-pointer rounded-l-[5rem] rounded-r-[1rem]',
+    };
 
-  useEffect(() => {
-    isSelected === 'btnL'
-      ? setBtnLSelected(selectedClass)
-      : setBtnLSelected('');
-    isSelected === 'btnCenter'
-      ? setBtnCenterSelected(selectedClass)
-      : setBtnCenterSelected('');
-    isSelected === 'btnR'
-      ? setBtnRSelected(selectedClass)
-      : setBtnRSelected('');
-    console.log(selectedClass);
-  }, [isSelected]);
+    if (isSelected) {
+      return selectedClass;
+    } else {
+      return defaultTabsClasses[currentTabLabel];
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-3xl font-bold text-white">הפעילות שלנו</h2>
       <div className="flex h-32 w-11/12 items-center justify-center gap-2">
         <HeadlineBtn
-          onClick={() => onClick('btnR', 'education')}
-          className={`relative rounded-l-[1rem] rounded-r-[5rem] ${getClassesSelecedTab(
-            activeLabel,
-            'education'
-          )}  ${btnR}`}
+          onClick={() => setActiveLabel('education')}
+          className={getClassesForTabBtn(activeLabel, 'education')}
           text="העשרה"
         />
         <HeadlineBtn
-          onClick={() => onClick('btnCenter', 'jobs')}
-          className={`rounded-lg focus:scale-x-150 ${getClassesSelecedTab(
-            activeLabel,
-            'jobs'
-          )} ${btnCenter}`}
+          onClick={() => setActiveLabel('jobs')}
+          className={getClassesForTabBtn(activeLabel, 'jobs')}
           text="מציאת עבודה"
         />
         <HeadlineBtn
-          onClick={() => onClick('btnL', 'projects')}
-          className={`rounded-l-[5rem] rounded-r-[1rem] ${getClassesSelecedTab(
-            activeLabel,
-            'projects'
-          )} ${btnLSelected}`}
+          onClick={() => setActiveLabel('projects')}
+          className={getClassesForTabBtn(activeLabel, 'projects')}
           text="פרוייקטים"
         />
       </div>
